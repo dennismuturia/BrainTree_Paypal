6e2cfa527d971e96fc6d2fa6863c311e1d3f6e2c
Dennis <dmuturia2922@gmail.com>
2018-08-29 07:20:50 +0300

    Added monolog and Sillex logging

--- index.php
+++ index.php
@@ -3,9 +3,15 @@ session_start();
 require('vendor/autoload.php');
 require_once ("braintree_init.php");
 
+$app = new Silex\Application();
 $app['debug'] = true;
 
-$app->get('/', function() {
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => 'php://stderr',
+  ));
+
+$app->get('/', function() use($app){
     return '<h2><?php echo ($clientTken = Braintree_Clienttoken::generate());?></h2>';
 });
 echo ($clientTken = Braintree_Clienttoken::generate());
