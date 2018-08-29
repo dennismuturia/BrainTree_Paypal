9281f8f4f989166b33f30f6c9564fecc520ec79d
Dennis <dmuturia2922@gmail.com>
2018-08-29 07:08:47 +0300

    Autoload added

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 session_start();
+require('vendor/autoload.php');
 require_once ("braintree_init.php");
 
+$app['debug'] = true;
+
+$app->get('/', function() {
+    return '<h2><?php echo ($clientTken = Braintree_Clienttoken::generate());?></h2>';
+});
 echo ($clientTken = Braintree_Clienttoken::generate());
+
+$app->run();
 ?>
+
